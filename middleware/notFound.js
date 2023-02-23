b011ce7436dd35832f69231f57c88b1181dbc4de
olamiq <qsamsonolalekan@gmail.com>
2023-02-23 03:35:34 -0800

    error route redirect

--- middleware/notFound.js
+++ middleware/notFound.js
@@ -1,4 +1,4 @@
 const notFound = (req, res) =>
-res.status(404).send('Route not found')
+res.status(404).send(`Rote not found,try <a href = '/api/v1/teams'>FOOTBALL API</a>`);
 
 module.exports = notFound
